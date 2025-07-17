2f0c95b89344f5dd8313ab5f033247946f8e31c8
Jakob <sokolov.yakov@inbox.ru>
2025-07-17 13:27:48 +0500

    added tasks 10.9.6 - 10.9.8

--- 10.9.6.Поиск отличников.py
+++ 10.9.6.Поиск отличников.py
@@ -5,12 +5,12 @@ import json
 
 async def main():
     res = []
-    async with aiofiles.open(r"C:\Users\Lenovo\Dev\asyncio\region_student\Задача Студенты", encoding='utf-8-sig') as file:
+    async with aiofiles.open(r"C:\Users\Lenovo\D9ev\asyncio\region_student\Задача Студенты", encoding='utf-8-sig') as file:
         reader = aiocsv.AsyncDictReader(file, delimiter=';')
         async for row in reader:
             res.append(row)
 
-    with open(r'C:\Users\Lenovo\Dev\asyncio\address_10000.json', mode='w', encoding='utf-8') as output:
+    with open(r'C:\Users\Lenovo\Dev\asyncio\auto', mode='w', encoding='utf-8') as output:
         await output.write(json.dump(res, ensure_ascii=False, indent=4))
         
 
--- test12.py
+++
@@ -1,3 +0,0 @@
-import datetime
-
-datetime.strptime('01.01.2018 04:50:13;125.200.11.141')
